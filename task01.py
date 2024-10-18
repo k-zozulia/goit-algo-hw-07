35da7583ed066000c709f5e2ec8df9d757fed84f
Karina <karina.zozulia23@gmail.com>
2024-10-18 19:53:25 +0300

    Added all tasks

---
+++ task01.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(self.root, value)
+
+    def _insert(self, current_node, value):
+        if value < current_node.value:
+            if current_node.left is None:
+                current_node.left = Node(value)
+            else:
+                self._insert(current_node.left, value)
+        elif value > current_node.value:
+            if current_node.right is None:
+                current_node.right = Node(value)
+            else:
+                self._insert(current_node.right, value)
+
+    def find_max(self):
+        if self.root is None:
+            return None
+        current_node = self.root
+        while current_node.right:
+            current_node = current_node.right
+        return current_node.value
+
+bst = BinarySearchTree()
+bst.insert(15)
+bst.insert(10)
+bst.insert(20)
+bst.insert(8)
+bst.insert(12)
+bst.insert(17)
+bst.insert(25)
+bst.insert(11)
+bst.insert(16)
+bst.insert(4)
+bst.insert(51)
+bst.insert(2)
+bst.insert(13)
+
+
+max_value = bst.find_max()
+print("Найбільше значення в дереві:", max_value)
